68e087201d0709b8ed57b0d52e3eb3f3e9798aa4
Laponn <naufal.sulthan.fakhry@gmail.com>
2026-05-13 14:46:11 +0700

    15. Membuat tampilan staggered grid  (Asesmen 2 MobPro)

--- app/src/main/java/com/naufalsulthanfakhry0092/asesmenmobpro1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/naufalsulthanfakhry0092/asesmenmobpro1/ui/screen/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.naufalsulthanfakhry0092.asesmenmobpro1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,8 +11,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
@@ -97,6 +104,7 @@ fun MainScreen(navController: NavHostController) {
         }
     ) { paddingValues ->
         ScreenContent(
+            showList = showList,
             modifier = Modifier.padding(paddingValues),
             navController = navController
         )
@@ -105,6 +113,7 @@ fun MainScreen(navController: NavHostController) {
 
 @Composable
 fun ScreenContent(
+    showList: Boolean,
     modifier: Modifier = Modifier,
     navController: NavHostController
 ) {
@@ -124,20 +133,38 @@ fun ScreenContent(
             Text(text = stringResource(id = R.string.list_kosong))
         }
     } else {
-        LazyColumn(
-            modifier = modifier.fillMaxSize(),
-            contentPadding = PaddingValues(bottom = 84.dp)
-        ) {
-            items(data) { tagihan ->
-                ListItem(
-                    tagihan = tagihan,
-                    onClick = {
-                        navController.navigate(Screen.FormUbah.withId(tagihan.id))
-                    }
-                )
-                HorizontalDivider()
+        if(showList){
+            LazyColumn(
+                modifier = modifier.fillMaxSize(),
+                contentPadding = PaddingValues(bottom = 84.dp)
+            ) {
+                items(data) { tagihan ->
+                    ListItem(
+                        tagihan = tagihan,
+                        onClick = {
+                            navController.navigate(Screen.FormUbah.withId(tagihan.id))
+                        }
+                    )
+                    HorizontalDivider()
+                }
             }
         }
+        else{
+            LazyVerticalStaggeredGrid(
+                modifier = modifier.fillMaxSize(),
+                columns = StaggeredGridCells.Fixed(2),
+                verticalItemSpacing = 8.dp,
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 84.dp)
+            ) {
+                items(data) {
+                    GridItem(tagihan = it) {
+                        navController.navigate(Screen.FormUbah.withId(it.id))
+                    }
+                }
+            }
+        }
+
     }
 }
 
@@ -204,5 +231,69 @@ fun ListItem(
 fun MainScreenPreview() {
     AsesmenMobpro1Theme {
         MainScreen(rememberNavController())
+    }
+}
+
+@Composable
+fun GridItem(
+    tagihan: Tagihan,
+    onClick: () -> Unit
+) {
+    val rupiah = NumberFormat.getCurrencyInstance(Locale.forLanguageTag("id-ID"))
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+        border = BorderStroke(width = 1.dp, DividerDefaults.color)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = tagihan.namaTagihan,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "Total: ${rupiah.format(tagihan.totalTagihan)}",
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Text(
+                text = "Jumlah orang: ${tagihan.jumlahOrang}",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Text(
+                text = if (tagihan.pakaiPajak) {
+                    "Pajak: ${tagihan.persentasePajak}%"
+                } else {
+                    "Tanpa pajak"
+                },
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Text(
+                text = "Per orang: ${rupiah.format(tagihan.hasilPerOrang)}",
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Text(
+                text = tagihan.tanggalDibuat,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
     }
 }
